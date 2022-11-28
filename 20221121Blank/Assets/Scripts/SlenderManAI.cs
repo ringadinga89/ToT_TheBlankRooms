3e1c9ca68da8c9cca453b1f3450164cd97183933
Holly <leeeholly@naver.com>
2022-11-28 17:26:24 +0900

    슬렌더맨 랜덤 이동 구현 완료

--- 20221121Blank/Assets/Scripts/SlenderManAI.cs
+++ 20221121Blank/Assets/Scripts/SlenderManAI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,7 +12,7 @@ public class SlenderManAI : MonoBehaviour
 
     public Transform[] arrWaypoint; // 목적지를 배열에 넣었음
 
-    private Vector3 destination;
+    private Vector3 destination; // 트렌스폼으로 바꿈***
     private Coroutine moveStop;
 
     // 시야각 및 추적 반경을 제어하는 EnemyFOV 클래스를 저장할 변수
@@ -33,7 +34,27 @@ public class SlenderManAI : MonoBehaviour
         Invoke("AiMove", 2);
     }
 
+    /*private void Update()
+    {
+        if (slenderFOV.isTracePlayer())
+        {
+            Run();
+        }
+        else if (first)
+        {
+            invoke AiMove();
+        }
+        else
+        {
+            First = false;
+        }
+    }*/
+
     // ***시야각에 들어오면 공격하게끔 만들어야 됨*** 
+    private void Run()
+    {
+
+    }
 
     private void AiMove()
     {
@@ -44,7 +65,7 @@ public class SlenderManAI : MonoBehaviour
         {
             if(i== random)
             {
-                this.destination = this.arrWaypoint[i].position; // 선정한 목적지를 기억
+                this.destination = this.arrWaypoint[i].position; // 선정한 목적지를 기억 작동 안 하고 있음 ***ㅏ 좌표가 0 0 0 으로 가는 거
 
                 if(this.moveStop == null)
                 {
@@ -66,15 +87,15 @@ public class SlenderManAI : MonoBehaviour
         {
             var dis = Vector3.Distance(this.transform.position, this.destination); // 목적지와 AI 사이의 거리 계산
 
-            if (dis <= 0.2f)
+            if (dis <= 0.5f)
             {
                 Debug.Log("목적지 도착");
-                this.anim.Play("Idle"); // 도착하면 애니메이션 바꿈
+                // this.anim.Play("Idle"); // 도착하면 애니메이션 바꿈
                 if(this.moveStop != null)
                 {
                     this.StopCoroutine(this.moveStop);
                     this.moveStop = null;
-                    Invoke("AiMove", 1.5f); // 1.5초 뒤 다른 곳으로 이동시킴
+                    Invoke("AiMove", 1.0f); // 1.5초 뒤 다른 곳으로 이동시킴
                     break;
                 }
             }
